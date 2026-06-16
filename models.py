5ede1056c7c2863898f910fb05200155a0ccfdd1
Lina Albasyoni <albasyoni21@gmail.com>
2026-06-16 13:05:28 +0300

    Remove unused models.py file

--- models.py
+++
@@ -1,8 +0,0 @@
-class Book:
-    def __init__(self, title, author, id=None):
-        self.id = id
-        self.title = title
-        self.author = author
-
-    def __repr__(self):
-        return f"Book({self.title}, {self.author})"
